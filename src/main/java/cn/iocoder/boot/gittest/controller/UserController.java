5c33471fdafbf4a04c286a0374c524eb44e6380c
AX <482915138@qq.com>
2024-08-14 09:27:10 +0800

    完成返回用户controller

---
+++ src/main/java/cn/iocoder/boot/gittest/controller/UserController.java
@@ -0,0 +1,17 @@
+package cn.iocoder.boot.gittest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Axing
+ * @description: 用户控制器
+ */
+@RestController
+public class UserController {
+
+    @GetMapping
+    public String getUser() {
+        return "Axing";
+    }
+}
